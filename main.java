f27383bb7bbd3239765f1e7fe6b0a2ff29b559f4
Jaydan-Miranda <miry08733@gmail.com>
2025-03-24 19:39:08 -0500

    first commit

---
+++ main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Temperature Converter");
+        System.out.print("Enter the temperature: ");
+        double fahrenheit = scanner.nextDouble();
+
+        // Convert F to C
+        double celsius = 5.0 / 9 * (fahrenheit - 32);
+        celsius = (Math.round(celsius*100.0)/100.0);
+        // Display result with two decimal places
+        System.out.println(Math.round(fahrenheit) + "F degree is equivalent to " + celsius + "C" );
+
+    }
+}
